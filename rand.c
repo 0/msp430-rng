653f34e60898f77a0d4a7b3dd358aef8586e547b
Dmitri Iouchtchenko <johnnyspoon@gmail.com>
2011-04-03 18:18:38 -0400

    Minor cleanup

--- rand.c
+++ rand.c
@@ -16,9 +16,15 @@ unsigned int rand() {
 	int i, j;
 	unsigned int result = 0;
 
+	/* Save state */
+	unsigned int TACCTL0_old = TACCTL0;
+	unsigned int TACTL_old = TACTL;
+
+	/* Set up timer */
 	TACCTL0 = CAP | CM_1 | CCIS_1;            // Capture mode, positive edge
 	TACTL = TASSEL_2 | MC_2;                  // SMCLK, continuous up
 
+	/* Generate bits */
 	for (i = 0; i < 16; i++) {
 		unsigned int ones = 0;
 
@@ -27,15 +33,19 @@ unsigned int rand() {
 
 			TACCTL0 &= ~CCIFG;                // Clear interrupt
 
-			if (1 & TACCR0)                   // If LSB set, count it
+			if (1 & TACCR0)                   // If LSb set, count it
 				ones++;
 		}
 
 		result >>= 1;                         // Save previous bits
 
 		if (ones >= 3)                        // Best out of 5
-			result |= 0x8000;                 // Set MSB
+			result |= 0x8000;                 // Set MSb
 	}
+
+	/* Restore state */
+	TACCTL0 = TACCTL0_old;
+	TACTL = TACTL_old;
 
 	return result;
 }
@@ -43,8 +53,8 @@ unsigned int rand() {
 /**
  * Pseudo-random number generator.
  *
- * 16-bit linear congruential generator.
- * NOTE: Only treat the upper byte of the return value as random.
+ * Implemented by a 16-bit linear congruential generator.
+ * NOTE: Only treat the MSB of the return value as random.
  *
  * @param state Previous state of the generator.
  * @return Next state of the generator.
